5a3d81c6041dca64d37372570a33d94b746de1c9
voleum <voleum@github.com>
2020-07-27 17:31:21 +0300

    ViewModels for RecycleView items

--- app/src/main/java/dev/voleum/speedruncom/api/SpeedrunComApi.kt
+++ app/src/main/java/dev/voleum/speedruncom/api/SpeedrunComApi.kt
@@ -1,11 +1,15 @@
 package dev.voleum.speedruncom.api
 
 import dev.voleum.speedruncom.model.GameList
+import dev.voleum.speedruncom.model.SeriesList
 import retrofit2.Call
 import retrofit2.http.GET
 
-interface GamesApi {
+interface SpeedrunComApi {
 
     @GET("/api/v1/games")
     fun games(): Call<GameList>
+
+    @GET("/api/v1/series")
+    fun series(): Call<SeriesList>
 }
---
+++ app/src/main/java/dev/voleum/speedruncom/ui/games/TabGamesFragment.kt
@@ -0,0 +1,56 @@
+package dev.voleum.speedruncom.ui.games
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dev.voleum.speedruncom.R
+import dev.voleum.speedruncom.databinding.FragmentTabGamesBinding
+import dev.voleum.speedruncom.enum.States
+
+class TabGamesFragment : Fragment() {
+
+    private lateinit var gamesViewModel: GamesViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        gamesViewModel = ViewModelProvider(this).get(GamesViewModel::class.java)
+        val binding: FragmentTabGamesBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_tab_games, null, false)
+        binding.viewModel = gamesViewModel
+        val root = binding.root
+        val recyclerView: RecyclerView = root.findViewById(R.id.games_recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(context, resources.getInteger(R.integer.games_columns))
+        recyclerView.adapter = gamesViewModel.adapter
+        checkData()
+        return root
+    }
+
+    private fun checkData() {
+//        if (view == null) return
+
+        when (gamesViewModel.state) {
+            States.CREATED -> {
+                gamesViewModel.setListener { checkData() }
+                gamesViewModel.load()
+            }
+            States.PROGRESS -> {
+                gamesViewModel.setListener { checkData() }
+            }
+            States.ERROR -> {
+                gamesViewModel.setListener { checkData() }
+                gamesViewModel.load()
+            }
+            States.LOADED -> {
+                gamesViewModel.adapter.addItems(gamesViewModel.data)
+            }
+        }
+    }
+}
