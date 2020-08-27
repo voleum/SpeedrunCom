692622859a01e6ba9f8e8ce46ae0aa2923e251ba
voleum <voleum@gmail.com>
2020-08-27 23:43:58 +0300

    Searching games complete

--- app/src/main/java/dev/voleum/speedruncom/adapter/SearchRecyclerViewAdapter.kt
+++ app/src/main/java/dev/voleum/speedruncom/adapter/SearchRecyclerViewAdapter.kt
@@ -6,9 +6,13 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import dev.voleum.speedruncom.GlideApp
 import dev.voleum.speedruncom.R
 import dev.voleum.speedruncom.databinding.HolderSearchBinding
-import dev.voleum.speedruncom.model.SearchResult
+import dev.voleum.speedruncom.model.Asset
+import dev.voleum.speedruncom.model.Game
 import dev.voleum.speedruncom.ui.nav.search.SearchItemViewModel
 
 class SearchRecyclerViewAdapter :
@@ -19,9 +23,7 @@ class SearchRecyclerViewAdapter :
     }
 
     lateinit var onEntryClickListener: OnEntryClickListener
-//    lateinit var trophyAssets: Assets
-//    var leaderboard: LeaderboardEmbed? = null
-    val searchResult = SearchResult(mutableListOf())
+    val searchResult = mutableListOf<Any>()
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -36,7 +38,7 @@ class SearchRecyclerViewAdapter :
             )
         )
 
-    override fun getItemCount(): Int = searchResult.games.size
+    override fun getItemCount(): Int = searchResult.size
 
     override fun onBindViewHolder(
         holder: SearchRecyclerViewAdapter.SearchViewHolder,
@@ -46,7 +48,13 @@ class SearchRecyclerViewAdapter :
     }
 
     override fun getItemId(position: Int): Long =
-        searchResult.games[position].hashCode().toLong()
+        searchResult[position].hashCode().toLong()
+
+    fun replaceItems(items: List<Any>) {
+        this.searchResult.clear()
+        this.searchResult.addAll(items)
+        notifyDataSetChanged()
+    }
 
     inner class SearchViewHolder(val binding: HolderSearchBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -58,88 +66,26 @@ class SearchRecyclerViewAdapter :
         }
 
         private val image: AppCompatImageView =
-            binding.root.findViewById(R.id.holder_leaderboard_image)
-
-        private val flag: AppCompatImageView =
-            binding.root.findViewById(R.id.holder_leaderboard_flag)
+            binding.root.findViewById(R.id.holder_search_image)
 
         fun bind(position: Int) {
 
             val viewModel =
-                SearchItemViewModel(
-//                    leaderboard!!.runs[position],
-//                    leaderboard!!.players.data.find {
-//                        it.id == leaderboard!!.runs[position].run.players[0].id
-//                    }
-                )
+                SearchItemViewModel(searchResult[position] as Game)
             binding.viewModel = viewModel
 
-//            val place = viewModel.placeValue
-//            if (place in 1..4) {
-//                loadImage(
-//                    when (place) {
-//                        1 -> trophyAssets.trophyFirst
-//                        2 -> trophyAssets.trophySecond
-//                        3 -> trophyAssets.trophyThird
-//                        else -> trophyAssets.trophyForth
-//                    }
-//                )
-//            }
-//            else clearImage()
-//            binding.holderLeaderboardConstraint.setBackgroundColor(
-//                if (position % 2 == 0) SpeedrunCom.instance.resources.getColor(android.R.color.transparent)
-//                else SpeedrunCom.instance.resources.getColor(R.color.colorPrimaryAlpha25)
-//            )
-//            when (viewModel.user?.nameStyle?.style) {
-//                UserNameStyles.SOLID.style -> binding.holderLeaderboardPlayer.setTextColor(
-//                    Color.parseColor(viewModel.user.nameStyle.color.light)
-//                )
-//                UserNameStyles.GRADIENT.style -> {
-//                    val tileMode = Shader.TileMode.CLAMP
-//                    val linearGradient = LinearGradient(
-//                        0.0F,
-//                        0.0F,
-//                        binding.holderLeaderboardPlayer.width.toFloat(),
-//                        binding.holderLeaderboardPlayer.textSize,
-//                        Color.parseColor(viewModel.user.nameStyle.colorFrom.light),
-//                        Color.parseColor(viewModel.user.nameStyle.colorTo.light),
-//                        tileMode
-//                    )
-//                    binding.holderLeaderboardPlayer.paint.shader = linearGradient
-//                }
-//            }
-//            val countryCode = viewModel.user?.location?.country?.code ?: ""
-//            if (countryCode.isNotEmpty())
-//                setFlag(countryCode)
+            loadImage(viewModel.imageAsset)
         }
 
-//        fun loadImage(asset: Asset?) {
-//            if (asset != null) {
-//                GlideApp.with(itemView)
-//                    .load(asset.uri)
-//                    .transition(DrawableTransitionOptions.withCrossFade())
-//                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-//                    .centerCrop()
-//                    .into(image)
-//            }
-//        }
-//
-//        fun clearImage() {
-//            GlideApp.with(itemView)
-//                .clear(image)
-//        }
-//
-//        fun setFlag(code: String) {
-//            GlideApp.with(itemView)
-//                .load(
-//                    SpeedrunCom.instance.resources.getIdentifier(
-//                        "flag_round_$code",
-//                        "drawable",
-//                        SpeedrunCom.instance.packageName
-//                    )
-//                )
-//                .into(flag)
-//        }
+        private fun loadImage(asset: Asset?) {
+            if (asset != null) {
+                GlideApp.with(itemView)
+                    .load(asset.uri)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                    .into(image)
+            }
+        }
     }
 
     interface OnEntryClickListener {
