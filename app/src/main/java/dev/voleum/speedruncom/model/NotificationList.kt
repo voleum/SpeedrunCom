ec8594f114dd11d793f0fcaa1bd203c4e7940b14
voleum <voleum@gmail.com>
2020-09-01 22:04:17 +0300

    Notification pagination

--- app/src/main/java/dev/voleum/speedruncom/model/NotificationList.kt
+++ app/src/main/java/dev/voleum/speedruncom/model/NotificationList.kt
@@ -1,3 +1,4 @@
 package dev.voleum.speedruncom.model
 
-data class NotificationList(val data: List<Notification>)
+data class NotificationList(val data: List<Notification>,
+                            val pagination: Pagination)
